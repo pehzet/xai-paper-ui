65aa9212be573eae27119548649e23a3ef9526de
Zmijewski <philipp.zmijewski@hs-osnabrueck.de>
2024-09-10 14:38:02 +0200

    paths modified

--- images.py
+++ images.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from PIL import Image
-
+import os
 def show_images():
-    image1 = Image.open(r"images\image_1.png")
-    image2 = Image.open(r"images\image_2.png")
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # aktuelles Verzeichnis
+    image1_path = os.path.join(base_dir, "images", "image_1.png")
+    image2_path = os.path.join(base_dir, "images", "image_2.png")
     col1, col2 = st.columns(2)
     with col1:
-        st.image(image1, caption="Global FI", use_column_width=True)
+        st.image(image1_path, caption="Global FI", use_column_width=True)
         
     with col2:
-        st.image(image2, caption="Local Explanation", use_column_width=True)
+        st.image(image2_path, caption="Local Explanation", use_column_width=True)
