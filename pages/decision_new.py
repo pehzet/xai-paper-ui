a2b14ed220c7a2bc874e483808a362c99118c05e
Zmijewski <philipp.zmijewski@hs-osnabrueck.de>
2025-06-11 11:54:57 +0200

    added note for dropdown

--- pages/decision_new.py
+++ pages/decision_new.py
@@ -20,7 +20,6 @@ def decision_new():
     st.markdown(f"<p style='font-size: 20px;'> Decision {st.session_state.decision_no} of 10 </p>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size: 20px;'> Your task is to decide which crop is the best to sow based on the given data in the table below and the charts on the right.<p>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size: 20px; color:red;'> On the lower right, you find the Cropify Decision Assistant that you are supposed to interact with to support your decision-making.<p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='font-size: 20px;'> Use the prediction on the left in the center to help you decide.<p>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size: 20px;'> Select the crop from the dropdown list and click on the Submit button. For further information check the diagrams on the top right. <p>", unsafe_allow_html=True)
     row1 = st.container(border=True)
     col1_1, col1_2 = st.columns(2, gap="small", border=True)
@@ -37,6 +36,7 @@ def decision_new():
     col3_1, col3_2 = st.columns(2, gap="small", border=True)
     with row3:
         with col3_1:
+            st.markdown("Select the crop you think is best from the dropdown list below. The percentages in brackets show the AI's predicted likelihood for each option.")
             decision_dropdown()
         with col3_2:
             st.markdown("**Cropify Decision Assistant** (scroll down to see the input field )")
