cbe5d872b5f1511d74b74a0ff71669527d4948bd
Zmijewski <philipp.zmijewski@hs-osnabrueck.de>
2025-07-03 14:29:35 +0200

    units added to table

--- pages/data_table.py
+++ pages/data_table.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 def show_data_table():
     st.markdown("Optimal Values for Parameters")
-    st.markdown("This table shows the optimal values for the parameters used in the prediction model.")
+    st.markdown("This table shows the quartils (Q1 and Q3) for the parameters used in the prediction model.")
     st.markdown("These values were determined based on the training data and are used to make predictions.")
     FILE_PATH = Path("prediction_model/data/optimal_ranges.csv")
 
@@ -13,9 +13,20 @@ def show_data_table():
 
     table.index.name = ""                   
     table.columns.names = ["Feature", ""]    
+    UNIT_MAP = {
+        "N":          "N (kg/ha)",
+        "P":          "P (kg/ha)",
+        "K":          "K (kg/ha)",
+        "temperature":"temperature (°C)",
+        "humidity":   "humidity (%)",
+        "ph":         "pH",                   #
+        "rainfall":   "rainfall (mm/month)",
+    }
 
+    # Multi-Header umbenennen – nur Ebene 0!
+    table_disp = table.rename(columns=UNIT_MAP, level=0)
     # 3) In Streamlit darstellen
-    st.dataframe(table)                    
+    st.dataframe(table_disp)                    
     st.markdown(
     """
     <style>
