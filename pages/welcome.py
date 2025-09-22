fb9620d6e9d909ff52a844a16af46902dba30f54
Philipp <philipp.zmijewski@hs-osnabrueck.de>
2025-09-22 10:34:42 +0200

    prolific length check and group type added

--- pages/welcome.py
+++ pages/welcome.py
@@ -6,11 +6,18 @@ def save_user_id_to_session_state(user_id):
         st.session_state["user_id"] = user_id
 def welcome_page():
     st.title("Welcome to Cropify!")
-    st.write("Enter your prolific ID and click the button below to start the experiment.")
+    st.write("Enter your Prolific ID and click the button below to start the experiment.")
 
     user_id = st.text_input("Enter your Prolific ID")
-    if st.button("Start Experiment"):
+
+    # Prüfen, ob Eingabe lang genug ist
+    valid = len(user_id) >= 6
+
+    if not valid:
+        st.info("Your Prolific ID must be at least 6 characters long.")
+
+    # Button wird deaktiviert, wenn Eingabe nicht gültig
+    if st.button("Start Experiment", disabled=not valid):
         save_user_id_to_session_state(user_id)
         st.session_state["page"] = "chat"
         st.rerun()
-
